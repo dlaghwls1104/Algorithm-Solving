ca7122e1487c54700ffbbbe710d897e364a90fdf
HOJIN <dlaghwls1104@naver.com>
2022-01-14 03:59:28 +0900

    백준_개미 수정필요

--- 시뮬레이션/3048.py
+++ 시뮬레이션/3048.py
@@ -7,11 +7,18 @@ for i in range(n,0,-1):
     list.append(first[i-1])
 for j in range(m):
     list.append(second[j])
-result="".join(list)
 if t == 0:
+    result="".join(list)
     print(result)
+elif t==1 :
+    for i in range(t):
+        list[n-1-i],list[n-i]=list[n-i],list[n-1-i]
+    result="".join(list)
+    print(result)
+
 else:
-    temp=result[n-1]
-    result[n-1]=result[m]
-    result[m]=temp
-    print(temp)
+    for i in range(t):
+        list[n-1-i],list[n-i]=list[n-i],list[n-1-i]
+        list[m-1],list[m-1+i]=list[m-1+i],list[m-1]
+    result="".join(list)
+    print(result)
