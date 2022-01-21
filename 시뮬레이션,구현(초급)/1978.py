c87ff0c1149eb3aa726316c532934ee7b190ab40
HOJIN <dlaghwls1104@naver.com>
2022-01-21 22:57:26 +0900

    백준_소수찾기

---
+++ 시뮬레이션,구현(초급)/1978.py
@@ -0,0 +1,17 @@
+from distutils.log import error
+
+
+n=int(input())
+prime=list(map(int,input().split(" ")))
+result=0
+for i in prime:
+    not_prime=0
+    if i>1:
+        for j in range(2,i):
+            if i%j == 0:
+                not_prime+=1           
+        if not_prime == 0:
+            result+=1
+
+print(result)
+
